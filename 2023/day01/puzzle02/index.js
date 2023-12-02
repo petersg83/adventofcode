d5f0a781247542c5ece02982c4c5d6bd5211308f
Pierre Noël <petersg83@gmail.com>
2023-12-02 01:10:42 +0100

    Reduce code size for day01 puzzle02

--- 2023/day01/puzzle02/index.js
+++ 2023/day01/puzzle02/index.js
@@ -20,19 +20,10 @@ const castToSingleStringDigit = (text) => {
   return text in digitMap ? digitMap[text] : text;
 };
 
-const getFirstDigit = (text) => {
-  const firstValue = Array.from(text.matchAll(DIGIT_AND_SPELLED_DIGIT_REGEX), (match) => match[1])[0];
-  return castToSingleStringDigit(firstValue);
-};
-
-const getLastDigit = (text) => {
-  const lastValue = Array.from(text.matchAll(DIGIT_AND_SPELLED_DIGIT_REGEX), (match) => match[1]).pop();
-  return castToSingleStringDigit(lastValue);
-};
-
 const getCalibrationValue = (text) => {
-  const firstDigit = getFirstDigit(text);
-  const lastDigit = getLastDigit(text);
+  const matches = Array.from(text.matchAll(DIGIT_AND_SPELLED_DIGIT_REGEX), (match) => match[1]);
+  const firstDigit = castToSingleStringDigit(matches[0]);
+  const lastDigit = castToSingleStringDigit(matches[matches.length - 1]);
   return Number(firstDigit + lastDigit);
 };
 
