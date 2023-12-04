246194b96fc3ad447c5881b753506fb8531d60a7
Pierre Noël <petersg83@gmail.com>
2023-12-04 17:10:59 +0100

    add day04 puzzle01

---
+++ 2023/day04/puzzle01/index.js
@@ -0,0 +1,12 @@
+import getLinesFromFile from "../../utils/get-lines-from-file.js";
+import fp from "https://esm.sh/lodash@4.17.21/fp.js";
+
+const countPointsFor = (card) => {
+  const [winningNumbers, personalNumbers] = card.split(':')[1].split('|').map(set => set.split(' ').filter(Boolean));
+  const nbOfWinners = fp.intersection(winningNumbers)(personalNumbers).length;
+  return nbOfWinners ? 2 ** (nbOfWinners - 1) : 0;
+};
+
+const inputLines = await getLinesFromFile('./data.txt');
+const sum = inputLines.reduce((sum, line) => sum + countPointsFor(line), 0);
+console.log(sum);
