c01688c6ba8a3d8d76b414f0e2d5d113fc3b1b31
Pierre Noël <petersg83@gmail.com>
2023-12-11 01:16:45 +0100

    add day09 puzzle02

---
+++ 2023/day09/puzzle2.js
@@ -0,0 +1,15 @@
+import getLinesFromFile from "../utils/get-lines-from-file.js";
+
+const getNextValue = (values) => {
+  if (values.every((v) => v === values[0])) return values[0];
+
+  const valuesDiffs = values.reduce((diffs, v, i) => i > 0 ? [...diffs, v - values[i - 1]] : diffs, []);
+  const nextValue = getNextValue(valuesDiffs);
+  return values[values.length - 1] + nextValue;
+};
+
+const inputLines = await getLinesFromFile('./data.txt');
+const metricsHistory = inputLines.map(l => l.split(' ').map(Number));
+const reversedMetricsHistory = metricsHistory.map(metricHistory => metricHistory.reverse());
+const sum = reversedMetricsHistory.reduce((sum, metricHistory) => sum + getNextValue(metricHistory), 0);
+console.log(sum);
